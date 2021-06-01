81e33d5d5346461e255f58951fdc7378a00b93ce
Kavina <kavina.17ee@kct.ac.in>
2021-06-01 13:48:11 +0530

    initial version

---
+++ src/main/java/M2EHANDSON/App.java
@@ -0,0 +1,25 @@
+package M2EHANDSON;
+
+import org.testng.annotations.Test;
+
+
+public class App 
+{	
+	
+	@Test
+	public void testng1() {
+		System.out.println( "906785" );
+	}
+	@Test
+	public void testng2() {
+		System.out.println( "906785" );
+	}
+	@Test
+	public void testng3() {
+		System.out.println( "906785" );
+	}
+	@Test
+	public void testng4() {
+		System.out.println( "906785" );
+	}
+}
